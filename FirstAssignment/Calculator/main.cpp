3ab42ee02627ad3f2809598b2e8677265ed1093d
Jin02 <quaggan02@gmail.com>
2015-03-11 21:55:26 +0900

    init with Xcode

---
+++ FirstAssignment/Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Jin on 2015. 3. 11..
+//  Copyright (c) 2015년 __Jin__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
