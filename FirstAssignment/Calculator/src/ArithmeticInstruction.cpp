df93a6b41965c2a62b27ba4a4530ba4ba6db09d8
Jin <quaggan02@gmail.com>
2015-03-14 04:19:37 +0900

    산술 연산 버그 수정

--- FirstAssignment/Calculator/src/ArithmeticInstruction.cpp
+++ FirstAssignment/Calculator/src/ArithmeticInstruction.cpp
@@ -17,32 +17,18 @@ void ArithmeticInstruction::Work()
 	ASSERT_COND_MSG(_operands.size() == 2, "Error, can not support current instruction yet");
 	{
 		System* system = System::GetInstance();
-		int registerIndex = 0;
         
-		const Operand& operand0 = _operands[0];
-        int operand1Data = GetData(_operands[1]);
-        
-        int result = 0;
-        
-        if( operand0.GetType() == Operand::Type::Register)
-		{
-			registerIndex = operand0.GetData();
-            result = Instruct(GetData(operand0), operand1Data);
-		}
-		else if( operand0.GetType() == Operand::Type::Value )
-		{
-            registerIndex = 0; //R0
-            result = Instruct(operand0.GetData(), operand1Data);
-        }
+        int operand0Data = GetData(_operands[0]);		
+		int operand1Data = GetData(_operands[1]);        
+        int result = Instruct(operand0Data, operand1Data);
         
 #ifdef USE_OUTPUT_DUMP_LOG
-        int operand0Data = GetData(_operands[0]);
         char buffer[256] = {0, };
-        sprintf(buffer, "R%d : %2d = %2d %2c %2d", registerIndex, result, operand0Data, GetDumpLogSymbol(), operand1Data);
+        sprintf(buffer, "R%d : %2d = %2d %2c %2d", 0, result, operand0Data, GetDumpLogSymbol(), operand1Data);
         
         DumpLogManager::GetInstance()->AddLog(buffer, true);
 #endif
         
-        system->SetDataToRegister(registerIndex, result);
+        system->SetDataToRegister(0, result);
 	}
 }
