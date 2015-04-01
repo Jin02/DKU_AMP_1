c1ea390d704bb104ea7e7e296158de7215a50c86
Jin02 <quaggan02@gmail.com>
2015-04-01 14:33:48 +0900

    버그 수

--- Calculator/Calculator/src/Multiply.cpp
+++ Calculator/Calculator/src/Multiply.cpp
@@ -21,7 +21,7 @@ unsigned int Multiply32BitRes::Instruct(unsigned int rsData, unsigned int rtData
 		GlobalDumpLogManager->AddLog("R[rd] = R[rs] * R[rt]", true);
 
 		char logBuffer[64] = {0, };
-		sprintf(logBuffer, "R[%d] = R[%d](0x%x) * R[%d](0x%x) = 0x%x", _rd, _rs, rsData, _rt, rtData,rsData + rtData);
+		sprintf(logBuffer, "R[%d] = R[%d](0x%x) * R[%d](0x%x) = 0x%x", _rd, _rs, rsData, _rt, rtData,rsData * rtData);
 		GlobalDumpLogManager->AddLog(logBuffer, true);
 		GlobalDumpManagerAddLog3NewLine;
 	}
